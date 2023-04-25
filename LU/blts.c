f72f87cb77b0326c0e1a3c28976519e8e74c239a
Sancho <sashamerkushev@yandex.ru>
2023-04-25 22:56:22 +0300

    b

--- LU/blts.c
+++ LU/blts.c
@@ -84,7 +84,7 @@ void blts(int ldmx, int ldmy, int ldmz, int nx, int ny, int nz, int k,
 
 
   //#pragma omp for schedule(static) nowait
-  //#pragma acc enter data create(tv[:ISIZ1][:5], tmat[:ISIZ1][:5][:5], vk[:ISIZ1][:ISIZ1][:5], ldy[:ISIZ1][:ISIZ1][:5][:5], ldx[:ISIZ1][:ISIZ1][:5][:5])
+  #pragma acc enter data create(tv[:ISIZ1][:5], tmat[:ISIZ1][:5][:5]) copyin(vk[:ISIZ1][:ISIZ1][:5], ldy[:ISIZ1][:ISIZ1][:5][:5], ldx[:ISIZ1][:ISIZ1][:5][:5])
   for (diag = jst; diag < jend; diag++) {
     #pragma acc parallel loop private(t, diag, i, j, m, tmp, tmp1)
     for (t = 0; t <= diag - jst; t++) {
@@ -209,6 +209,7 @@ void blts(int ldmx, int ldmy, int ldmz, int nx, int ny, int nz, int k,
       vk[j][i][0] = tv[j][0] / tmat[j][0][0];
     }
   }
+  #pragma acc exit data delete(tv[:ISIZ1][:5], tmat[:ISIZ1][:5][:5]) copyout(vk[:ISIZ1][:ISIZ1][:5], ldy[:ISIZ1][:ISIZ1][:5][:5], ldx[:ISIZ1][:ISIZ1][:5][:5])
   for (diag = jst + 1; diag < jend; diag++) {
     //#pragma acc parallel loop private(t, diag, i, j, m, tmp, tmp1)
     for (t = 0; t <= (jend - jst) - diag; t++) {
