b589b97e5413a62e665695f8773e46bd4928d4e3
Sancho <sashamerkushev@yandex.ru>
2023-04-30 03:21:40 +0300

    b

--- LU/blts.c
+++ LU/blts.c
@@ -85,7 +85,8 @@ void blts(int ldmx, int ldmy, int ldmz, int nx, int ny, int nz, int k,
 
   //#pragma omp for schedule(static) nowait
   for (diag = jst; diag < jend; diag++) {
-    #pragma acc parallel loop private(t, diag, i, j, m, tmp, tmp1)
+    //#pragma acc parallel loop private(t, diag, i, j, m, tmp, tmp1)
+    #pragma acc kernels
     for (t = 0; t <= diag - jst; t++) {
       j = diag - t;
       i = jst + t;
@@ -209,7 +210,7 @@ void blts(int ldmx, int ldmy, int ldmz, int nx, int ny, int nz, int k,
     }
   }
   for (diag = jst + 1; diag < jend; diag++) {
-    #pragma acc parallel loop private(t, diag, i, j, m, tmp, tmp1)
+    //#pragma acc parallel loop private(t, diag, i, j, m, tmp, tmp1)
     for (t = 0; t <= (jend - jst) - diag; t++) {
       j = jend - 1 - t;
       i = diag + t;
