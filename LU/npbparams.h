6f13f121e180d6302e6b0863a49f3a82b45fd7b2
Sancho <sashamerkushev@yandex.ru>
2023-05-01 04:37:00 +0300

    b

--- LU/npbparams.h
+++ LU/npbparams.h
@@ -1,4 +1,4 @@
-/* CLASS = A */
+/* CLASS = W */
 /*
    This file is generated automatically by the setparams utility.
    It sets the number of processors and the class of the NPB
@@ -6,17 +6,17 @@
 */
 
 /* full problem size */
-#define ISIZ1  64
-#define ISIZ2  64
-#define ISIZ3  64
+#define ISIZ1  33
+#define ISIZ2  33
+#define ISIZ3  33
 
 /* number of iterations and how often to print the norm */
-#define ITMAX_DEFAULT  250
-#define INORM_DEFAULT  250
-#define DT_DEFAULT     2.0
+#define ITMAX_DEFAULT  300
+#define INORM_DEFAULT  300
+#define DT_DEFAULT     1.5e-3
 
 #define CONVERTDOUBLE  false
-#define COMPILETIME "30 Apr 2023"
+#define COMPILETIME "01 May 2023"
 #define NPBVERSION "3.3.1"
 #define CS1 "nvc"
 #define CS2 "$(CC)"
