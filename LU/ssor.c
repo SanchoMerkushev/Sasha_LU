f9730702c86477d71e3f51a2b553ca872fc2664a
Sancho <sashamerkushev@yandex.ru>
2023-05-02 14:07:15 +0300

    t

--- LU/ssor.c
+++ LU/ssor.c
@@ -474,7 +474,7 @@ void ssor(int niter)
 			  //#pragma omp for schedule(static) nowait
 			  //#pragma acc data create(tmat_blts[:ISIZ1][:5][:5], tv_blts[:ISIZ1][:5])
 			  for (diag = jst; diag < jend; diag++) {
-			    #pragma acc parallel loop private(t, diag, i, j, m, tmp_blts, tmp1_blts)
+			    #pragma acc parallel loop private(t, i, j, m, tmp_blts, tmp1_blts)
 			    for (t = 0; t <= diag - jst; t++) {
 			      j = diag - t;
 			      i = jst + t;
@@ -599,7 +599,7 @@ void ssor(int niter)
 			  }
 			  //#pragma acc data create(tmat_blts[:ISIZ1][:5][:5], tv_blts[:ISIZ1][:5])
 			  for (diag = jst + 1; diag < jend; diag++) {
-			    #pragma acc parallel loop private(t, diag, i, j, m, tmp_blts, tmp1_blts)
+			    #pragma acc parallel loop private(t, i, j, m, tmp_blts, tmp1_blts)
 			    for (t = 0; t <= (jend - jst) - diag; t++) {
 			      j = jend - 1 - t;
 			      i = diag + t;
@@ -1096,7 +1096,7 @@ void ssor(int niter)
 				  //#pragma acc parallel loop private(i, j, m, tmp_buts, tmp1_buts)
 				  //#pragma acc data create(tmat_buts[:ISIZ1][:5][:5])
 				  for (diag = jend - 1; diag > jst; diag--) {
-				    #pragma acc parallel loop private(i, j, m, tmp_buts, tmp1_buts, diag, t)
+				    #pragma acc parallel loop private(i, j, m, tmp_buts, tmp1_buts, t)
 				    for (t = 0; t <= (jend - jst) - diag; t++) {
 				       j = jend - 1 - t;
 				       i = diag + t;
@@ -1224,7 +1224,7 @@ void ssor(int niter)
 				  }
 				  //#pragma acc data create(tmat_buts[:ISIZ1][:5][:5])
 				  for (diag = jend  - 1; diag >= jst; diag--) {
-				    #pragma acc parallel loop private(i, j, m, tmp_buts, tmp1_buts, diag, t)
+				    #pragma acc parallel loop private(i, j, m, tmp_buts, tmp1_buts, t)
 				    for (t = 0; t <= diag - jst; t++) {
 				      j = diag - t;
 				      i = jst + t;
