e23bf5ec1b651b72a7e9cd79be59a041ec8479a7
Sancho <sashamerkushev@yandex.ru>
2023-05-03 03:32:40 +0300

    w

--- LU/ssor.c
+++ LU/ssor.c
@@ -672,6 +672,7 @@ void ssor(int niter)
       // end blts( ISIZ1, ISIZ2, ISIZ3, nx, ny, nz, k, omega, rsd, a, b, c, d, ist, iend, jst, jend, nx0, ny0 );
     } // end k_first
     }
+    printf("%f\n", rsd[3][4][5][2]);
     for (k = nz - 2; k > 0; k--) { // start k_second
       // start jacu(k);
 	  double r43;
