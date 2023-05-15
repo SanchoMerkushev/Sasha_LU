d703c664d05350fc7d6e4d1451897d627a1acf01
Sancho <sashamerkushev@yandex.ru>
2023-05-15 17:51:02 +0300

    t

--- LU/npbparams.h
+++ LU/npbparams.h
@@ -1,4 +1,4 @@
-/* CLASS = W */
+/* CLASS = B */
 /*
    This file is generated automatically by the setparams utility.
    It sets the number of processors and the class of the NPB
@@ -6,22 +6,22 @@
 */
 
 /* full problem size */
-#define ISIZ1  33
-#define ISIZ2  33
-#define ISIZ3  33
+#define ISIZ1  102
+#define ISIZ2  102
+#define ISIZ3  102
 
 /* number of iterations and how often to print the norm */
-#define ITMAX_DEFAULT  300
-#define INORM_DEFAULT  300
-#define DT_DEFAULT     1.5e-3
+#define ITMAX_DEFAULT  250
+#define INORM_DEFAULT  250
+#define DT_DEFAULT     2.0
 
 #define CONVERTDOUBLE  false
-#define COMPILETIME "05 May 2023"
+#define COMPILETIME "15 May 2023"
 #define NPBVERSION "3.3.1"
-#define CS1 "pgcc"
+#define CS1 "nvc"
 #define CS2 "$(CC)"
 #define CS3 "-lm"
 #define CS4 "-I../common"
-#define CS5 "-O3 -acc -ta=multicore -Minfo=all  -mcmodel..."
-#define CS6 "-O3 -acc -ta=multicore -Minfo=all  -mcmodel..."
+#define CS5 "-O3 -acc -ta=nvidia -Minfo=all  -mcmodel=me..."
+#define CS6 "-O3 -acc -ta=nvidia -Minfo=all  -mcmodel=me..."
 #define CS7 "randdp"
