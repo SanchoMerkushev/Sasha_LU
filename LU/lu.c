ea01055ec781cefc43bf9796e716917679ac107b
Sancho <sashamerkushev@yandex.ru>
2023-05-17 02:33:15 +0300

    t

--- LU/lu.c
+++ LU/lu.c
@@ -72,16 +72,16 @@ double dt, omega, tolrsd[5], rsdnm[5], errnm[5], frc, ttotal;
 int itmax, invert;
 
 /* common/cjac/ */
-//double a[ISIZ2][ISIZ1/2*2+1][5][5];
-//double b[ISIZ2][ISIZ1/2*2+1][5][5];
-//double c[ISIZ2][ISIZ1/2*2+1][5][5];
-//double d[ISIZ2][ISIZ1/2*2+1][5][5];
+double a[ISIZ2][ISIZ1/2*2+1][5][5];
+double b[ISIZ2][ISIZ1/2*2+1][5][5];
+double c[ISIZ2][ISIZ1/2*2+1][5][5];
+double d[ISIZ2][ISIZ1/2*2+1][5][5];
 
 /* common/cjacu/ */
-//double au[ISIZ2][ISIZ1/2*2+1][5][5];
-//double bu[ISIZ2][ISIZ1/2*2+1][5][5];
-//double cu[ISIZ2][ISIZ1/2*2+1][5][5];
-//double du[ISIZ2][ISIZ1/2*2+1][5][5];
+double au[ISIZ2][ISIZ1/2*2+1][5][5];
+double bu[ISIZ2][ISIZ1/2*2+1][5][5];
+double cu[ISIZ2][ISIZ1/2*2+1][5][5];
+double du[ISIZ2][ISIZ1/2*2+1][5][5];
 
 
 //---------------------------------------------------------------------
