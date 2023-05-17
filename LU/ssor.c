f837b84bd0c8b19f13cd35b67bf63dccc63de0e5
Sancho <sashamerkushev@yandex.ru>
2023-05-17 04:08:26 +0300

    t

--- LU/ssor.c
+++ LU/ssor.c
@@ -68,14 +68,14 @@ void ssor(int niter)
   }
   timer_start(1);
 printf("BEFORE\n");
-/*double a[ISIZ2][ISIZ1/2*2+1][5][5];
+double a[ISIZ2][ISIZ1/2*2+1][5][5];
 double b[ISIZ2][ISIZ1/2*2+1][5][5];
 double c[ISIZ2][ISIZ1/2*2+1][5][5];
 double d[ISIZ2][ISIZ1/2*2+1][5][5];
 double au[ISIZ2][ISIZ1/2*2+1][5][5];
 double bu[ISIZ2][ISIZ1/2*2+1][5][5];
 double cu[ISIZ2][ISIZ1/2*2+1][5][5];
-double du[ISIZ2][ISIZ1/2*2+1][5][5];*/
+double du[ISIZ2][ISIZ1/2*2+1][5][5];
 //double ****a, ****b, ****c, ****d,****au, ****bu, ****cu, ****du;
 printf("AFTER\n");
   //---------------------------------------------------------------------
