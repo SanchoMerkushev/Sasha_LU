2ac7867e07b0e4658b8bb8ab20e4a5db0944d46c
Sancho <sashamerkushev@yandex.ru>
2023-05-17 03:32:23 +0300

    t

--- LU/ssor.c
+++ LU/ssor.c
@@ -67,7 +67,16 @@ void ssor(int niter)
     timer_clear(i);
   }
   timer_start(1);
-
+printf("BEFORE\n);
+double a[ISIZ2][ISIZ1/2*2+1][5][5];
+double b[ISIZ2][ISIZ1/2*2+1][5][5];
+double c[ISIZ2][ISIZ1/2*2+1][5][5];
+double d[ISIZ2][ISIZ1/2*2+1][5][5];
+double au[ISIZ2][ISIZ1/2*2+1][5][5];
+double bu[ISIZ2][ISIZ1/2*2+1][5][5];
+double cu[ISIZ2][ISIZ1/2*2+1][5][5];
+double du[ISIZ2][ISIZ1/2*2+1][5][5];
+printf("AFTER\n);
   //---------------------------------------------------------------------
   // the timestep loop
   //---------------------------------------------------------------------
