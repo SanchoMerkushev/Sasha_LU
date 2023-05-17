397d918b1f2cf8e681e8941090c4a2085c550290
Sancho <sashamerkushev@yandex.ru>
2023-05-17 03:43:25 +0300

    t

--- LU/ssor.c
+++ LU/ssor.c
@@ -68,14 +68,15 @@ void ssor(int niter)
   }
   timer_start(1);
 printf("BEFORE\n");
-extern double a[ISIZ2][ISIZ1/2*2+1][5][5];
-extern double b[ISIZ2][ISIZ1/2*2+1][5][5];
-extern double c[ISIZ2][ISIZ1/2*2+1][5][5];
-extern double d[ISIZ2][ISIZ1/2*2+1][5][5];
-extern double au[ISIZ2][ISIZ1/2*2+1][5][5];
-extern double bu[ISIZ2][ISIZ1/2*2+1][5][5];
-extern double cu[ISIZ2][ISIZ1/2*2+1][5][5];
-extern double du[ISIZ2][ISIZ1/2*2+1][5][5];
+/*double a[ISIZ2][ISIZ1/2*2+1][5][5];
+double b[ISIZ2][ISIZ1/2*2+1][5][5];
+double c[ISIZ2][ISIZ1/2*2+1][5][5];
+double d[ISIZ2][ISIZ1/2*2+1][5][5];
+double au[ISIZ2][ISIZ1/2*2+1][5][5];
+double bu[ISIZ2][ISIZ1/2*2+1][5][5];
+double cu[ISIZ2][ISIZ1/2*2+1][5][5];
+double du[ISIZ2][ISIZ1/2*2+1][5][5];*/
+double ****a, ****b, ****c, ****d,****au, ****bu, ****cu, ****du;
 printf("AFTER\n");
   //---------------------------------------------------------------------
   // the timestep loop
